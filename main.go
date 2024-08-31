c9ace7dcec8145b0776da0c8dd3ac5ea5fb347d5
s19835 <s19835@sci.pdn.ac.lk>
2024-08-31 21:00:06 +0530

    add: json file config

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
@@ -15,20 +17,31 @@ type items struct {
 func main() {
 	c := colly.NewCollector()
 
+	var courses []items
+
 	// c.OnHTML("div[data-test-course-name]", func(h *colly.HTMLElement) {
 	// 	fmt.Println(h.ChildText("span"))
 	// })
 
 	c.OnHTML("a[data-test-course-card]", func(h *colly.HTMLElement) {
-		courses := items{
+		course := items{
 			Name:        h.ChildText("div[data-test-course-name]"),
 			Description: h.ChildText("div[data-test-course-description]"),
 			Stages:      h.ChildText("span[class='text-xs text-gray-500']"),
 		}
-		fmt.Println(courses)
+
+		courses = append(courses, course)
 	})
 
 	c.Visit("https://app.codecrafters.io/catalog")
+
+	content, err := json.Marshal(courses)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	os.WriteFile("codecrafters-catalog.json", content, 0644)
 
 	fmt.Println("webscraper 1.0 ", c)
 }
